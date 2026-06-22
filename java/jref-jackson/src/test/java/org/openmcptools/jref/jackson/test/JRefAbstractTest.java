acadf0ddad601fde74a45e533fd3ec0cf343f0e0
Scott Lewis <scottslewis@gmail.com>
2026-06-22 13:23:34 -0700

    Added tests, removed old tests, refactored test for
    clarity and coverage

--- java/jref-jackson/src/test/java/org/openmcptools/jref/jackson/test/JRefAbstractTest.java
+++ java/jref-jackson/src/test/java/org/openmcptools/jref/jackson/test/JRefAbstractTest.java
@@ -11,33 +11,35 @@ import tools.jackson.databind.json.JsonMapper;
 
 public class JRefAbstractTest {
 
-    public static JsonMapper.Builder jsonMapperBuilder() {
-        return JsonMapper.builder();
-    }
+	public static boolean TRACE = false;
 
-	protected ObjectMapper buildObjectMapperWithJRefSupport() {
+	public static JsonMapper.Builder jsonMapperBuilder() {
+		return JsonMapper.builder();
+	}
+
+	protected ObjectMapper buildObjectMapperJRef() {
 		return jsonMapperBuilder().addModule(new JRefModule()).build();
 	}
 
-	protected ObjectMapper buildObjectMapperWithoutJRefSupport() {
+	protected ObjectMapper buildObjectMapperNoJRef() {
 		return jsonMapperBuilder().build();
 	}
 
-    static long countMatches(String text, String target) {
-        if (text == null || target == null || target.isEmpty()) return 0;
-        String quotedTarget = Pattern.quote(target); 
-        
-        return Pattern.compile(quotedTarget)
-                      .matcher(text)
-                      .results() 
-                      .count();  
-    }
-    
-	protected void assertJRefCount(String input, long expectedJRefs) {
-		assertEquals(expectedJRefs, countMatches(input,"$ref"));
+	static long countMatches(String text, String target) {
+		if (text == null || target == null || target.isEmpty())
+			return 0;
+		String quotedTarget = Pattern.quote(target);
+
+		return Pattern.compile(quotedTarget).matcher(text).results().count();
 	}
-	
+
+	protected void assertJRefCount(String input, long expectedJRefs) {
+		assertEquals(expectedJRefs, countMatches(input, "$ref"));
+	}
+
 	void trace(String method, String s) {
-		System.out.println(method+"."+s);
+		if (TRACE) {
+			System.out.println(method + "." + s);
+		}
 	}
 }
